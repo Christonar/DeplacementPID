810fb2089d297592d78b3c004f913fdb303a1c59
Christonar <christonar@hotmail.fr>
2021-10-28 12:59:02 -0400

    Accéleration/Décélération marche
    Reste juste à tweaker valeurs PID

--- DeplacementPID/src/PID.cpp
+++ DeplacementPID/src/PID.cpp
@@ -5,41 +5,57 @@ void AvancerPID(uint32_t nbrPulse)
     ENCODER_Reset(RIGHT);
     ENCODER_Reset(LEFT);
     uint32_t compteRight = 0;
+    uint32_t compteRightTotal = 0;
     uint32_t compteLeft = 0;
     uint32_t ancienneCorrection = 0;
-    static uint32_t correction_P = 0;
-    static uint32_t correction_I = 0;
-    static uint32_t correction_D = 0;
+    uint32_t correction_P = 0;
+    uint32_t correction_I = 0;
+    uint32_t correction_D = 0;
 
     float vitesse = 0.1;
     MOTOR_SetSpeed(RIGHT,vitesse);
     MOTOR_SetSpeed(LEFT,vitesse);
 
     uint16_t compteDelai = 0;
-    while(compteRight < nbrPulse)
+    while(compteRightTotal < nbrPulse)
     {
-        if (compteRight > 1400)
+        //Serial.print("Compte right ");
+        //Serial.println(compteRightTotal,DEC);
+        if (compteRightTotal > (nbrPulse-700))
+        {
+            vitesse = 0.1;
+            MOTOR_SetSpeed(RIGHT,vitesse);
+            //Serial.println("Vitesse 0.1");
+        }
+        else if ((compteRightTotal > 1400) && (compteRightTotal < (nbrPulse-1400)))
         {
             vitesse = 0.5;
+            MOTOR_SetSpeed(RIGHT,vitesse);
+            //Serial.println("Vitesse 0.5");
         }
-        else if (compteRight > 700)
+        else if ((compteRightTotal > (nbrPulse-1400)) || (compteRightTotal > 700))
         {
             vitesse = 0.3;
+            MOTOR_SetSpeed(RIGHT,vitesse);
+            //Serial.println("Vitesse 0.3");
         }
 
         compteDelai++;
-        compteRight = ENCODER_Read(RIGHT);
-        if(compteDelai > 20)
+        if(compteDelai > 10)
         {
-            compteLeft = ENCODER_Read(LEFT);
+            compteRight = ENCODER_ReadReset(RIGHT);
+            compteRightTotal += compteRight;
+            compteLeft = ENCODER_ReadReset(LEFT);
             ancienneCorrection = correction_P;
             correction_P = compteRight - compteLeft;
             correction_I += correction_P;
             correction_D = correction_P - ancienneCorrection;
-            MOTOR_SetSpeed(LEFT,(vitesse + 0.3*correction_P + 0.3*correction_I + 0.3*correction_D));
+            MOTOR_SetSpeed(LEFT,(vitesse + 0.5*correction_P + 0.4*correction_I + 0.2*correction_D));
             compteDelai = 0;
         }
     }
+    MOTOR_SetSpeed(RIGHT,0);
+    MOTOR_SetSpeed(LEFT,0);
     ENCODER_Reset(RIGHT);
     ENCODER_Reset(LEFT);
 }
--- DeplacementPID/src/main.cpp
+++ DeplacementPID/src/main.cpp
@@ -6,9 +6,9 @@
 void setup() {
   // put your setup code here, to run once:
   BoardInit();
+  AvancerPID(14000); // Avancer 1 metre 700 pulses = 5cm 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  AvancerPID(140000); // Avancer 1 metre 700 pulses = 5cm 
 }
